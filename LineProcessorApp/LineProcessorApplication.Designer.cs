18c4c53995305d7338905bb95528113a34934f61
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2013-06-03 15:23:13 -0500

    Change default font settings.

--- LineProcessorApp/LineProcessorApplication.Designer.cs
+++ LineProcessorApp/LineProcessorApplication.Designer.cs
@@ -30,128 +30,139 @@ namespace LineProcessorApp
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.btnQuit = new System.Windows.Forms.ToolStripMenuItem();
-			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-			this.ctlProcessor = new LineProcessorApp.Controls.ProcessingView();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.ctlInput = new LineProcessorApp.Views.Input();
-			this.groupBox2 = new System.Windows.Forms.GroupBox();
-			this.ctlOutput = new LineProcessorApp.Views.Output();
-			this.menuStrip1.SuspendLayout();
-			this.tableLayoutPanel1.SuspendLayout();
-			this.groupBox1.SuspendLayout();
-			this.groupBox2.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// menuStrip1
-			// 
-			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.btnQuit = new System.Windows.Forms.ToolStripMenuItem();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.ctlProcessor = new LineProcessorApp.Controls.ProcessingView();
+      this.ctlInput = new LineProcessorApp.Views.Input();
+      this.ctlOutput = new LineProcessorApp.Views.Output();
+      this.menuStrip1.SuspendLayout();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.groupBox1.SuspendLayout();
+      this.groupBox2.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
-			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(843, 24);
-			this.menuStrip1.TabIndex = 0;
-			this.menuStrip1.Text = "menuStrip1";
-			// 
-			// fileToolStripMenuItem
-			// 
-			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 3, 0, 3);
+      this.menuStrip1.Size = new System.Drawing.Size(983, 25);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btnQuit});
-			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-			this.fileToolStripMenuItem.Text = "&File";
-			// 
-			// btnQuit
-			// 
-			this.btnQuit.Name = "btnQuit";
-			this.btnQuit.Size = new System.Drawing.Size(97, 22);
-			this.btnQuit.Text = "&Quit";
-			this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
-			// 
-			// tableLayoutPanel1
-			// 
-			this.tableLayoutPanel1.ColumnCount = 2;
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel1.Controls.Add(this.ctlProcessor, 0, 0);
-			this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 1);
-			this.tableLayoutPanel1.Controls.Add(this.groupBox2, 1, 1);
-			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
-			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.RowCount = 2;
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(843, 675);
-			this.tableLayoutPanel1.TabIndex = 1;
-			// 
-			// ctlProcessor
-			// 
-			this.tableLayoutPanel1.SetColumnSpan(this.ctlProcessor, 2);
-			this.ctlProcessor.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.ctlProcessor.Location = new System.Drawing.Point(3, 3);
-			this.ctlProcessor.Name = "ctlProcessor";
-			this.ctlProcessor.Size = new System.Drawing.Size(837, 331);
-			this.ctlProcessor.TabIndex = 0;
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Controls.Add(this.ctlInput);
-			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.groupBox1.Location = new System.Drawing.Point(3, 340);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(415, 332);
-			this.groupBox1.TabIndex = 1;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Input";
-			// 
-			// ctlInput
-			// 
-			this.ctlInput.Contents = "Line 1\r\nLine 2\r\nLine 3\r\n";
-			this.ctlInput.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.ctlInput.Location = new System.Drawing.Point(3, 16);
-			this.ctlInput.Name = "ctlInput";
-			this.ctlInput.Size = new System.Drawing.Size(409, 313);
-			this.ctlInput.TabIndex = 0;
-			// 
-			// groupBox2
-			// 
-			this.groupBox2.Controls.Add(this.ctlOutput);
-			this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.groupBox2.Location = new System.Drawing.Point(424, 340);
-			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(416, 332);
-			this.groupBox2.TabIndex = 2;
-			this.groupBox2.TabStop = false;
-			this.groupBox2.Text = "Output";
-			// 
-			// ctlOutput
-			// 
-			this.ctlOutput.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.ctlOutput.Location = new System.Drawing.Point(3, 16);
-			this.ctlOutput.Name = "ctlOutput";
-			this.ctlOutput.Size = new System.Drawing.Size(410, 313);
-			this.ctlOutput.TabIndex = 1;
-			// 
-			// LineProcessorApplication
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(843, 699);
-			this.Controls.Add(this.tableLayoutPanel1);
-			this.Controls.Add(this.menuStrip1);
-			this.MainMenuStrip = this.menuStrip1;
-			this.Name = "LineProcessorApplication";
-			this.Text = "Line Processor";
-			this.menuStrip1.ResumeLayout(false);
-			this.menuStrip1.PerformLayout();
-			this.tableLayoutPanel1.ResumeLayout(false);
-			this.groupBox1.ResumeLayout(false);
-			this.groupBox2.ResumeLayout(false);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 19);
+      this.fileToolStripMenuItem.Text = "&File";
+      // 
+      // btnQuit
+      // 
+      this.btnQuit.Name = "btnQuit";
+      this.btnQuit.Size = new System.Drawing.Size(97, 22);
+      this.btnQuit.Text = "&Quit";
+      this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.ColumnCount = 2;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.Controls.Add(this.ctlProcessor, 0, 0);
+      this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 1);
+      this.tableLayoutPanel1.Controls.Add(this.groupBox2, 1, 1);
+      this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 25);
+      this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 2;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(983, 781);
+      this.tableLayoutPanel1.TabIndex = 1;
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.ctlInput);
+      this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.groupBox1.Location = new System.Drawing.Point(3, 394);
+      this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.groupBox1.Size = new System.Drawing.Size(485, 383);
+      this.groupBox1.TabIndex = 1;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Input";
+      // 
+      // groupBox2
+      // 
+      this.groupBox2.Controls.Add(this.ctlOutput);
+      this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.groupBox2.Location = new System.Drawing.Point(494, 394);
+      this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.groupBox2.Size = new System.Drawing.Size(486, 383);
+      this.groupBox2.TabIndex = 2;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "Output";
+      // 
+      // ctlProcessor
+      // 
+      this.tableLayoutPanel1.SetColumnSpan(this.ctlProcessor, 2);
+      this.ctlProcessor.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.ctlProcessor.Location = new System.Drawing.Point(3, 4);
+      this.ctlProcessor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.ctlProcessor.Name = "ctlProcessor";
+      this.ctlProcessor.Size = new System.Drawing.Size(977, 382);
+      this.ctlProcessor.TabIndex = 0;
+      // 
+      // ctlInput
+      // 
+      this.ctlInput.Contents = "Line 1\r\nLine 2\r\nLine 3\r\n";
+      this.ctlInput.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.ctlInput.Location = new System.Drawing.Point(3, 20);
+      this.ctlInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.ctlInput.Name = "ctlInput";
+      this.ctlInput.Size = new System.Drawing.Size(479, 359);
+      this.ctlInput.TabIndex = 0;
+      // 
+      // ctlOutput
+      // 
+      this.ctlOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.ctlOutput.Location = new System.Drawing.Point(3, 20);
+      this.ctlOutput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.ctlOutput.Name = "ctlOutput";
+      this.ctlOutput.Size = new System.Drawing.Size(480, 359);
+      this.ctlOutput.TabIndex = 1;
+      // 
+      // LineProcessorApplication
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(983, 806);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Controls.Add(this.menuStrip1);
+      this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.MainMenuStrip = this.menuStrip1;
+      this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.Name = "LineProcessorApplication";
+      this.Text = "Line Processor";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox2.ResumeLayout(false);
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
 		}
 
--- LineProcessorApp/Views/ProcessingLine.Designer.cs
+++ LineProcessorApp/Views/ProcessingLine.Designer.cs
@@ -28,83 +28,89 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.pnlLayout = new System.Windows.Forms.TableLayoutPanel();
-			this.txtValue = new System.Windows.Forms.TextBox();
-			this.txtExpression = new System.Windows.Forms.TextBox();
-			this.txtName = new System.Windows.Forms.TextBox();
-			this.errExpression = new System.Windows.Forms.ErrorProvider(this.components);
-			this.pnlLayout.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.errExpression)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// pnlLayout
-			// 
-			this.pnlLayout.AutoSize = true;
-			this.pnlLayout.ColumnCount = 4;
-			this.pnlLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.pnlLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.pnlLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.pnlLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-			this.pnlLayout.Controls.Add(this.txtValue, 2, 0);
-			this.pnlLayout.Controls.Add(this.txtExpression, 1, 0);
-			this.pnlLayout.Controls.Add(this.txtName, 0, 0);
-			this.pnlLayout.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.pnlLayout.Location = new System.Drawing.Point(0, 0);
-			this.pnlLayout.Margin = new System.Windows.Forms.Padding(0);
-			this.pnlLayout.Name = "pnlLayout";
-			this.pnlLayout.RowCount = 1;
-			this.pnlLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.pnlLayout.Size = new System.Drawing.Size(338, 26);
-			this.pnlLayout.TabIndex = 0;
-			// 
-			// txtValue
-			// 
-			this.txtValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+      this.components = new System.ComponentModel.Container();
+      this.pnlLayout = new System.Windows.Forms.TableLayoutPanel();
+      this.txtValue = new System.Windows.Forms.TextBox();
+      this.txtExpression = new System.Windows.Forms.TextBox();
+      this.txtName = new System.Windows.Forms.TextBox();
+      this.errExpression = new System.Windows.Forms.ErrorProvider(this.components);
+      this.pnlLayout.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.errExpression)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // pnlLayout
+      // 
+      this.pnlLayout.AutoSize = true;
+      this.pnlLayout.ColumnCount = 4;
+      this.pnlLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+      this.pnlLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+      this.pnlLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+      this.pnlLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.pnlLayout.Controls.Add(this.txtValue, 2, 0);
+      this.pnlLayout.Controls.Add(this.txtExpression, 1, 0);
+      this.pnlLayout.Controls.Add(this.txtName, 0, 0);
+      this.pnlLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.pnlLayout.Location = new System.Drawing.Point(0, 0);
+      this.pnlLayout.Margin = new System.Windows.Forms.Padding(0);
+      this.pnlLayout.Name = "pnlLayout";
+      this.pnlLayout.RowCount = 1;
+      this.pnlLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      this.pnlLayout.Size = new System.Drawing.Size(326, 25);
+      this.pnlLayout.TabIndex = 0;
+      // 
+      // txtValue
+      // 
+      this.txtValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.txtValue.Location = new System.Drawing.Point(215, 3);
-			this.txtValue.Name = "txtValue";
-			this.txtValue.ReadOnly = true;
-			this.txtValue.Size = new System.Drawing.Size(100, 20);
-			this.txtValue.TabIndex = 3;
-			// 
-			// txtExpression
-			// 
-			this.txtExpression.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+      this.txtValue.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.txtValue.Location = new System.Drawing.Point(205, 1);
+      this.txtValue.Margin = new System.Windows.Forms.Padding(1);
+      this.txtValue.Name = "txtValue";
+      this.txtValue.ReadOnly = true;
+      this.txtValue.Size = new System.Drawing.Size(100, 23);
+      this.txtValue.TabIndex = 3;
+      // 
+      // txtExpression
+      // 
+      this.txtExpression.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.txtExpression.Location = new System.Drawing.Point(109, 3);
-			this.txtExpression.Name = "txtExpression";
-			this.txtExpression.Size = new System.Drawing.Size(100, 20);
-			this.txtExpression.TabIndex = 2;
-			// 
-			// txtName
-			// 
-			this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+      this.txtExpression.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.txtExpression.Location = new System.Drawing.Point(103, 1);
+      this.txtExpression.Margin = new System.Windows.Forms.Padding(1);
+      this.txtExpression.Name = "txtExpression";
+      this.txtExpression.Size = new System.Drawing.Size(100, 23);
+      this.txtExpression.TabIndex = 2;
+      // 
+      // txtName
+      // 
+      this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.txtName.Location = new System.Drawing.Point(3, 3);
-			this.txtName.Name = "txtName";
-			this.txtName.Size = new System.Drawing.Size(100, 20);
-			this.txtName.TabIndex = 1;
-			// 
-			// errExpression
-			// 
-			this.errExpression.ContainerControl = this;
-			// 
-			// ProcessingLine
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.AutoSize = true;
-			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.Controls.Add(this.pnlLayout);
-			this.Margin = new System.Windows.Forms.Padding(0);
-			this.Name = "ProcessingLine";
-			this.Size = new System.Drawing.Size(338, 26);
-			this.pnlLayout.ResumeLayout(false);
-			this.pnlLayout.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.errExpression)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+      this.txtName.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.txtName.Location = new System.Drawing.Point(1, 1);
+      this.txtName.Margin = new System.Windows.Forms.Padding(1);
+      this.txtName.Name = "txtName";
+      this.txtName.Size = new System.Drawing.Size(100, 23);
+      this.txtName.TabIndex = 1;
+      // 
+      // errExpression
+      // 
+      this.errExpression.ContainerControl = this;
+      // 
+      // ProcessingLine
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoSize = true;
+      this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.Controls.Add(this.pnlLayout);
+      this.Margin = new System.Windows.Forms.Padding(0);
+      this.Name = "ProcessingLine";
+      this.Size = new System.Drawing.Size(326, 25);
+      this.pnlLayout.ResumeLayout(false);
+      this.pnlLayout.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.errExpression)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
 		}
 
